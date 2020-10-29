178fa5f5b9a3645f7c997e5a9e228f262437b48e
nuruliislam <mdnrl59@gmail.com>
2020-10-29 04:06:29 +0000

    adding TestController.java to test the project

---
+++ src/myPackage/springdemo/controller/TestController.java
@@ -0,0 +1,16 @@
+package myPackage.springdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/request")
+public class TestController {
+	
+	@RequestMapping("/test")
+	public String listRequest(Model model)
+	{
+		return "list-request";
+	}
+}
