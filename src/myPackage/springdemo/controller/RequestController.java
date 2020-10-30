9eefe11880e05535cfc2fff8e02b3d4b84552e72
nuruliislam <mdnrl59@gmail.com>
2020-10-30 02:12:44 +0000

    inject request service to RequestController

--- src/myPackage/springdemo/controller/RequestController.java
+++ src/myPackage/springdemo/controller/RequestController.java
@@ -6,27 +6,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import myPackage.springdemo.dao.RequestDAO;
 import myPackage.springdemo.entity.BloodRequest;
+import myPackage.springdemo.service.RequestService;
 
 @Controller
 @RequestMapping("/request")
 public class RequestController {
 	
 	@Autowired
-	private RequestDAO requestDAO;
+	private RequestService requestService;
 	
 	@GetMapping("/list")
 	public String listRequests(Model theModel) {
 		
 		// get customers from the service
-		List<BloodRequest> theRequests = requestDAO.getRequests();
+		List<BloodRequest> theRequests = requestService.getRequests();
 				
 		// add the customers to the model
 		theModel.addAttribute("requests", theRequests);
 		
 		return "request_view";
 	}
-
+	
+	
 }
